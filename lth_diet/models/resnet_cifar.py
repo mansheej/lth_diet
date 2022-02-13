fcc8c2e82d2b2786bfddaec49e61098399ac0e9c
mansheej <mansheej@stanford.edu>
2022-02-13 22:34:48 +0000

    docs

--- lth_diet/models/resnet_cifar.py
+++ lth_diet/models/resnet_cifar.py
@@ -8,17 +8,19 @@ from composer.models.resnets import CIFAR_ResNet
 
 @dataclass
 class ResNetCIFAR(ModelHparams):
-    num_layers: int = hp.optional(doc="Number of layers, supported: 20, 56", default=20)
-    num_classes: int = hp.optional(doc="Number of classes", default=10)
+    num_layers: int = hp.optional(
+        doc="Number of layers, currently supported: {20, 56}, default=20", default=20
+    )
+    num_classes: int = hp.optional(doc="Number of classes, default=10", default=10)
     initializers: List[Initializer] = hp.optional(
-        doc="The initialization strategy for the model",
+        doc="Model initialization strategy, default=[KAIMING_NORMAL, BN_UNIFORM]",
         default_factory=lambda: [Initializer.KAIMING_NORMAL, Initializer.BN_UNIFORM],
     )
 
     def validate(self):
         super().validate()
         if self.num_layers not in [20, 56]:
-            raise ValueError("Currently supported: num_layers = 20 | 56")
+            raise ValueError("Currently supported: num_layers in [20, 56]")
 
     def initialize_object(self) -> ComposerClassifier:
         model = CIFAR_ResNet.get_model_from_name(
