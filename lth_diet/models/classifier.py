47816785c3821315c52121586dbd881971f890da
mansheej <mansheej@stanford.edu>
2022-02-20 02:28:06 +0000

    ClassifierHparams

---
+++ lth_diet/models/classifier.py
@@ -0,0 +1,14 @@
+import abc
+from dataclasses import dataclass
+
+import yahp as hp
+from composer.models.base import ComposerClassifier
+
+
+@dataclass
+class ClassifierHparams(hp.Hparams, abc.ABC):
+    num_classes: int = hp.required("Number of classes")
+
+    @abc.abstractmethod
+    def initialize_object(self) -> ComposerClassifier:
+        pass
