211c92386b6223ce3a6a0167d985bcec6d527a47
mansheej <mansheej@stanford.edu>
2022-02-22 18:08:20 +0000

    reformat utils

--- lth_diet/utils/utils.py
+++ lth_diet/utils/utils.py
@@ -10,10 +10,11 @@ HparamField = Union[int, float, bool, str, Enum, List, hp.Hparams]
 
 def resolve_field_name(field: HparamField) -> str:
     if isinstance(field, hp.Hparams):
-        if hasattr(field, "name"):
-            name = field.name
-        else:
-            name = get_hparams_name(field, type(field).__name__, [])
+        name = (
+            field.name
+            if hasattr(field, "name")
+            else get_hparams_name(field, type(field).__name__, [])
+        )
     elif isinstance(field, List):
         name = f"[{','.join([resolve_field_name(x) for x in field])}]"
     else:
@@ -23,7 +24,9 @@ def resolve_field_name(field: HparamField) -> str:
 
 def get_hparams_name(hparams: hp.Hparams, prefix: str, ignore_fields: List[str]) -> str:
     field_names = [field.name for field in fields(hparams)]
+    # filter out ignore_fields
     field_names = [f for f in field_names if f not in ignore_fields]
+    # filter out any fields that are None
     field_names = [f for f in field_names if getattr(hparams, f) is not None]
     field_names = [
         f"{f}={resolve_field_name(getattr(hparams, f))}" for f in field_names
