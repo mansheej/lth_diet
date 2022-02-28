394837f52167283288c5ce6df7374d4dcf59ba14
mansheej <mansheej@stanford.edu>
2022-02-28 05:23:50 +0000

    clean implementation of standard training

--- lth_diet/exps/train.py
+++ lth_diet/exps/train.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
 from dataclasses import dataclass
+import os
+from pathlib import Path
+import torch
 from typing import List, Optional
 
 import yahp as hp
@@ -75,7 +79,7 @@ class TrainExperiment(hp.Hparams):
     device: DeviceHparams = hp.optional("Default: gpu", default=GPUDeviceHparams())
     precision: Precision = hp.optional("Default: amp", default=Precision.AMP)
     dataloader: DataloaderHparams = hp.optional("Default: Mosaic defaults", default=DataloaderHparams())
-    save_interval: Optional[str] = hp.optional("Default: 1ep, None: no saving", default="1ep")
+    # save_interval: Optional[str] = hp.optional("Default: 1ep, None: no saving", default="1ep")
     get_name: bool = hp.optional("Print name and exit. Default: False", default=False)
 
     @property
@@ -97,45 +101,51 @@ class TrainExperiment(hp.Hparams):
         if self.val_batch_size % world_size != 0:
             raise ValueError(f"Val batch size not divisible by number of processes")
 
-    def _get_trainer(self, exp_id: str) -> Trainer:
-        # get device
+    def _exists_in_bucket(self) -> bool:
+        return False
+
+    def _setup_exp(self) -> Path | str:
+        run_dir = Path(os.environ["COMPOSER_RUN_DIRECTORY"])
+        exp_dir = run_dir / f"{utils.get_hash(self.name)}/replicate_{self.replicate}/main"
+        os.makedirs(exp_dir, exist_ok=True)
+        with open(exp_dir / "hparams.yaml", "w") as f:
+            f.write(self.to_yaml())
+        return exp_dir
+
+    def run(self) -> None:
+        # Abort if a completed exp exists in the bucket, else make local exp dir and save hparams
+        if self._exists_in_bucket():
+            return
+        exp_dir = self._setup_exp()
+
+        # Device
         device = self.device.initialize_object()
 
-        # train data
-        reproducibility.seed_all(42)  # prevent unwanted randomness in data generation
-        train_device_batch_size = self.train_batch_size // dist.get_world_size()
-        train_dataloader = self.train_data.initialize_object(train_device_batch_size, self.dataloader)
-        # validation data
-        val_device_batch_size = self.val_batch_size // dist.get_world_size()
-        val_dataloader = self.val_data.initialize_object(val_device_batch_size, self.dataloader)
-
-        # model
+        # Initialize and save initial model
         seed = self.seed * (self.replicate + 1)
         reproducibility.seed_all(seed)
         model = self.model.initialize_object()
+        torch.save(model.state_dict(), exp_dir / "ckpt_init.pt")
 
-        # optimizer and scheduler
+        # Load training and validation data
+        reproducibility.seed_all(42)  # prevent unwanted randomness in data generation
+        train_device_batch_size = self.train_batch_size // dist.get_world_size()
+        train_dataloader = self.train_data.initialize_object(train_device_batch_size, self.dataloader)
+        val_device_batch_size = self.val_batch_size // dist.get_world_size()
+        val_dataloader = self.val_data.initialize_object(val_device_batch_size, self.dataloader)
+
+        # Initialize optimizer and schedulers
         optimizer = self.optimizer.initialize_object(model.parameters())
         schedulers = [x.initialize_object() for x in self.schedulers]
 
-        # algorithms, callbacks
-        if self.algorithms is None:
-            self.algorithms = []
-        algorithms = [x.initialize_object() for x in self.algorithms]
+        # Initialize algorithms and callbacks
+        algorithms = [] if self.algorithms is None else [x.initialize_object() for x in self.algorithms]
         callbacks = [x.initialize_object() for x in self.callbacks]
 
-        # checkpointing
-        save_folder = f"{exp_id}/replicate_{self.replicate}/main"
-        if self.save_interval is None:
-            self.save_interval = f"{len(train_dataloader)}ba"
-
-        # loggers
-        for logger in self.loggers:
-            if isinstance(logger, WandBLoggerHparams):
-                logger.name = exp_id
+        # Initialize Loggers
         loggers = [x.initialize_object(config=self.to_dict()) for x in self.loggers]
 
-        # trainer
+        # Initialize trainer
         trainer = Trainer(
             model=model,
             train_dataloader=train_dataloader,
@@ -149,12 +159,10 @@ class TrainExperiment(hp.Hparams):
             seed=seed,
             loggers=loggers,
             callbacks=callbacks,
-            save_folder=save_folder,
-            save_interval=self.save_interval,
         )
-        return trainer
 
-    def run(self) -> None:
-        exp_id = utils.get_hash(self.name)
-        trainer = self._get_trainer(exp_id)
+        # Train model
         trainer.fit()
+
+        # Save final model
+        torch.save(model.state_dict(), exp_dir / "ckpt_final.pt")
