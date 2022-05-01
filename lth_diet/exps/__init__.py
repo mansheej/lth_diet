3ce79e16537c38cd700cfb0629846d3f9dd5ce31
mansheej <mansheej@stanford.edu>
2022-05-01 04:11:11 +0000

    lottery

--- lth_diet/exps/__init__.py
+++ lth_diet/exps/__init__.py
@@ -1,2 +1,3 @@
+from lth_diet.exps.lottery import LotteryExperiment
 from lth_diet.exps.pretrain_train import PretrainAndTrainExperiment
 from lth_diet.exps.train import TrainExperiment
---
+++ run/lottery.py
@@ -0,0 +1,32 @@
+import dotenv
+from lth_diet.exps import LotteryExperiment as Experiment
+from lth_diet.utils import utils
+import sys
+
+
+dotenv.load_dotenv()
+
+
+def main() -> None:
+    if len(sys.argv) == 1:
+        sys.argv = [sys.argv[0], "--help"]
+
+    exp = Experiment.create(cli_args=True)
+
+    print("\n" + ("=" * 80))
+    print("\nEXPERIMENT NAME:\n" + exp.name)
+    print("\nREPLICATE: " + str(exp.replicate))
+    print("\nEXPERIMENT HASH:\n" + utils.get_hash(exp.name))
+
+    if exp.get_name:
+        print("\n" + ("=" * 80) + "\n")
+        sys.exit(0)
+    print("\n" + ("-" * 80) + "\n")
+
+    exp.run()
+
+    print("\n" + ("=" * 80) + "\n")
+
+
+if __name__ == "__main__":
+    main()
