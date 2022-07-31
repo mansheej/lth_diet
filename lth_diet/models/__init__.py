544c6451696ddb2bdd505ee829310c614764ad0f
bwlarsen <Brett.Larsen14@gmail.com>
2022-07-31 00:07:17 +0000

    First attempt at VGG

--- lth_diet/models/__init__.py
+++ lth_diet/models/__init__.py
@@ -1,5 +1,6 @@
 from lth_diet.models.classifier import ClassifierHparams
 from lth_diet.models.resnet import ResNetClassifierHparams
 from lth_diet.models.resnet_cifar import ResNetCIFARClassifierHparams
+from lth_diet.models.vgg_cifar import VGG16CIFARClassifierHparams
 
-model_registry = {"resnet_cifar": ResNetCIFARClassifierHparams, "resnet": ResNetClassifierHparams}
+model_registry = {"resnet_cifar": ResNetCIFARClassifierHparams, "resnet": ResNetClassifierHparams, "vgg16": VGG16CIFARClassifierHparams}
